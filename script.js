69503e217427c612f8de50bd33b4cafc4ad6b85a
amitblogger <amitblogger999@gmail.com>
2025-04-27 20:55:13 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-
-// JavaScript logic for section switching, quiz, tips, blog, theme toggle, popups
-
-document.addEventListener('DOMContentLoaded', function () {
-  // Implement logic from previous steps
-});
